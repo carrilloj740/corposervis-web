296818215c315526174a4c7535186a50d8232c78
Jose <josemanuel.co@gmail.com>
2024-11-05 17:09:41 -0300

    Modificaciones routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { SadmimComponent } from './components/sistemas/sadmim/sadmim.component';
-import { SadmipComponent } from './components/sistemas/sadmip/sadmip.component';
-import { SadmitComponent } from './components/sistemas/sadmit/sadmit.component';
-import { ServiDescComponent } from './components/servicios/servi-desc/servi-desc.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent },
-  {path: 'sadmim', component: SadmimComponent},
-  {path: 'sadmip', component: SadmipComponent},
-  {path: 'sadmit', component: SadmitComponent},
-  {path: 'servicios', component: ServiDescComponent},
+import { ServiDescComponent } from './components/servicios/servi-desc/servi-desc.component';
+import { SadmipComponent } from './components/sistemas/sadmip/sadmip.component';
+import { SadmimComponent } from './components/sistemas/sadmim/sadmim.component';
+import { SadmitComponent } from './components/sistemas/sadmit/sadmit.component';
+import { SistemasComponent } from './components/sistemas/sistemas.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'sadmim', component: SadmimComponent },
+  { path: 'sadmip', component: SadmipComponent },
+  { path: 'sadmit', component: SadmitComponent },
+  { path: 'servicios', component: ServiDescComponent },
+  { path: 'sistemas', component: SistemasComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+
+
+
+
 
 ];
 
